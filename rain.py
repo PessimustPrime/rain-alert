02d7a0163ec767c9f1dd6e0e5fc877c2b1bd9f0b
Pessimust Prime <68779293+PessimustPrime@users.noreply.github.com>
2022-12-11 19:51:55 +0530

    Update rain.py

--- rain.py
+++ rain.py
@@ -1,9 +1,9 @@
 import requests
 import smtplib
 
-API_key = # open weather map api key
-lat = # latitude of the place
-lon = # longitude of the place
+API_key = '03ff24b4b33bfc05092e79f411160734'
+lat = 12.971599# 23.1815
+lon = 77.594566 # 79.9864
 parameter = {
     'lat': lat,
     'lon': lon,
@@ -21,21 +21,35 @@ will_rain = False
 for hour in hourly_data:
 
     condition_code = hour['weather'][0]['id']
+
     if condition_code < 700:
         will_rain = True
-    print(f'{hour}:{will_rain}')
+
 
 if will_rain:
-    email = '' # sender's email
-    password = '' # senders's password (16 digit from 2 factor auth after mid 2022)
-    to_address = '' # reciver's email id
-    
+    email = 'trialt872@gmail.com'
+    password = 'xktvdtdqtnayztsj'
+
     connection = smtplib.SMTP('smtp.gmail.com', 587, timeout=120)
     connection.starttls()
     connection.login(user=email, password=password)
 
-    connection.sendmail(from_addr=email, to_addrs='',
+    connection.sendmail(from_addr=email, to_addrs='troublewood18@gmail.com',
                         msg=f'subject:It is going to rain\n\nBring umbrella')
+
+    connection.close()
+    print(f"Mail has been sent to successfully \nRain!!!!")
+
+else:
+    email = 'trialt872@gmail.com'
+    password = 'xktvdtdqtnayztsj'
+
+    connection = smtplib.SMTP('smtp.gmail.com', 587, timeout=120)
+    connection.starttls()
+    connection.login(user=email, password=password)
+
+    connection.sendmail(from_addr=email, to_addrs='troublewood18@gmail.com',
+                        msg=f'subject:It is NOT going to rain\n\nHave a beautiful Day!')
 
     connection.close()
     print(f"Mail has been sent to successfully")
